3e01e2d84d831b886bf0d19b550e9d3f69e54579
Melvin Kimathi <karutikimathi@MKIMATHI-ENG.local>
2024-02-20 08:16:45 +0300

    signup and signin routes

--- expenses-manager-api/init.py
+++ expenses-manager-api/init.py
@@ -2,6 +2,9 @@ from flask import Flask
 import os
 from src.config.config import Config
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from src.routes import api
 
 # load env variables
 load_dotenv()
@@ -14,3 +17,19 @@ config = Config().dev_config
 
 # make the app to use dev env
 app.env = config.ENV
+
+# Path for our local sql lite database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+# sqlachemy instance
+db = SQLAlchemy(app)
+
+# Flask Migrate instance to handle migrations
+migrate = Migrate(app, db)
+
+# import models to let the migrate tool know
+# TODO: import the various migrations
+
+# import api blueprint to register it with app
+app.register_blueprint(api, url_prefix = "/api")
+
---
+++ expenses-manager-api/src/controllers/users.py
@@ -0,0 +1,22 @@
+from flask import request, Response, json, Blueprint
+
+# user controller blueprint to be registered with api blueprint
+users = Blueprint("users", __name__)
+
+# login route
+@users.route('/signin', methods = ["GET", "POST"])
+def handle_login():
+    return Response(
+        response=json.dumps({'status': "signin success"}),
+        status=200,
+        mimetype='application/json'
+    )
+    
+# signup route
+@users.route('/signup', methods = ["GET", "POST"])
+def handle_signup():
+    return Response(
+        response=json.dumps({'status': "signup success"}),
+        status=200,
+        mimetype='application/json'
+    )
