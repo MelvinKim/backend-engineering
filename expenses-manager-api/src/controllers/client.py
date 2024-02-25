12e72d2523afb304bb705b2a8d2490e3cab6c045
Melvin Kimathi <karutikimathi@MKIMATHI-ENG.local>
2024-02-25 18:08:20 +0300

    Implement redis caching

--- expenses-manager-api/src/controllers/client.py
+++ expenses-manager-api/src/controllers/client.py
@@ -1,13 +1,13 @@
 from flask import request, Response, jsonify, json, Blueprint, make_response, jsonify
 from src.models.client import Client
-from src import db
-from src.utils import cleanup
+from src import db, cache
+
 
 clients = Blueprint("clients", __name__)
 
-# get client
 @clients.route("/", methods=["GET"])
 def get_clients():
+    """ Get clients """
     try:
         current_clients = Client.query.all()
         return make_response(jsonify([client.serialize() for client in current_clients]), 200)
@@ -18,9 +18,9 @@ def get_clients():
             500
         )
 
-# create client
 @clients.route("/", methods=["POST"])
 def create_client():
+    """ Create client """
     try:
         new_client = Client(
             firstname = request.json['firstname'],
@@ -30,6 +30,9 @@ def create_client():
         )
         db.session.add(new_client)
         db.session.commit()
+        
+        cache.set(new_client.id, json.dumps(new_client.serialize()))
+        
         message = "new client created successfully"
         return make_response(
             jsonify({"message": message}),
@@ -45,17 +48,25 @@ def create_client():
 # get client by id
 @clients.route("/<client_id>", methods=["GET"])
 def get_client_by_id(client_id):
-    response = Client.query.get(client_id).serialize()
+    """ Get client by id"""
+    response = cache.get(client_id)
+    if not response:
+        response = Client.query.get(client_id).serialize()
+        cache.set(client_id, json.dumps(response))
+        return jsonify(response)
     return jsonify(response)
 
 # update client details
 @clients.route("/<client_id>", methods=["PUT"])
 def update_client(client_id):
+    """ Update client details """
     client = Client.query.get(client_id)
     client.firstname = request.json["firstname"]
     client.lastname = request.json["lastname"]
     client.email = request.json["email"]
     db.session.commit()
+
+    cache.set(client_id, json.dumps(client))
 
     response = Client.query.get(client_id).serialize()
     return jsonify(response)
@@ -63,8 +74,11 @@ def update_client(client_id):
 # safe delete client details
 @clients.route("/<client_id>", methods=["DELETE"])
 def delete_client(client_id):
+    """ Delete client """
     client = Client.query.get(client_id)
     client.is_active = False
     db.session.commit()
+    
+    cache.set(client_id, client)
 
     return ('Client with Id "{}" deleted successfully!').format(client_id)
